ed0212820415903d6edc28a709b502b7eb5677e9
ssy <new01270@naver.com>
2020-10-16 10:47:26 +0900

    first jab

---
+++ morningjava/src/Morning1016.java
@@ -0,0 +1,33 @@
+public class Morning1016{
+    public static void main(String[] args) {
+        System.out.println("Java Sample");
+
+        int[][] regAry = new int[5][5]; 
+        int cnt = 1;
+         
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                regAry[i][j] = cnt++;
+            }
+        }
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                System.out.printf("%3d", regAry[j][i]);
+            }
+            System.out.println();
+        }
+
+        for(int j = 0; j < 5; j++) {
+            for(int i = 0; i < 5; i++) {
+                regAry[j][i] = cnt++;
+            }
+        }
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++)
+                System.out.printf("%3d", regAry[j][i]);
+        }
+        System.out.println();
+    }
+}
