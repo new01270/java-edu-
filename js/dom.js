11e5397f7929fd2752c6bb17f499c0f9f8fa3203
ssy <new01270@naver.com>
2020-10-26 15:01:04 +0900

    201026.js

---
+++ js/dom.js
@@ -0,0 +1,13 @@
+// console.log(window);
+// let text = this.document.children[1].children[1].innerText;
+console.log(this.document.childNodes[1]);//html
+console.log(this.document.childNodes[1].childNodes[0]);//head
+console.log(this.document.childNodes[1].childNodes[1]);//text
+console.log(this.document.childNodes[1].childNodes[2]);//body
+console.log(this.document.childNodes[1].childNodes[2].children[0]);//div=result
+console.log(this.document.childNodes[1].childNodes[2].children[1]);//input
+
+
+
+
+//this: window.
